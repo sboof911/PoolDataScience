c99a6f69731d46ba2c6842e0380e092655e983e4
sboof911 <amaach@student.1337.ma>
2024-04-19 10:35:54 +0100

    little changes

--- Day00/ex01/script.sh
+++ Day00/ex01/script.sh
@@ -2,5 +2,5 @@ curl -fsS https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --d
 sudo sh -c 'echo "deb [signed-by=/usr/share/keyrings/packages-pgadmin-org.gpg] https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 sudo apt install pgadmin4 -y
 sudo apt install pgadmin4-desktop -y
-sudo apt-get install python3-dev
-sudo apt-get install build-essential
+sudo apt install python3-dev -y
+sudo apt install build-essential -y
